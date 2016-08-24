277f6c4730a8f58c8d0013d18a66bfc58988af68
Shuai Yang <shuai.yang@hpe.com>
2016-08-24 13:49:11 +0800

    服务管理代码上传

---
+++ src/test/java/com/hpe/devops/tests/ApplicationTests.java
@@ -0,0 +1,29 @@
+package com.hpe.devops.tests;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = ConfigServerApplication.class)
+//@WebAppConfiguration
+//@IntegrationTest("server.port=0")
+public class ApplicationTests {
+
+/*	@Value("${local.server.port}")
+	private int port = 0;
+
+	@Test
+	public void configurationAvailable() {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = new TestRestTemplate().getForEntity(
+				"http://localhost:" + port + "/app/cloud", Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+
+	@Test
+	public void envPostAvailable() {
+		MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>();
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = new TestRestTemplate().postForEntity(
+				"http://localhost:" + port + "/admin/env", form, Map.class);
+		assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
+*/
+}
